07a7e51682347ee71c7f73e79c301125224a8016
Martin Chabot <chabotm@gmail.com>
2015-08-21 15:55:43 -0400

    Adding elm.js

--- treeDisplay.elm
+++ treeDisplay.elm
@@ -5,20 +5,30 @@ import Tree exposing (..)
 import Color
 import Text
 import Graphics.Element exposing (..)
+import Debug
 
-displayTree: (Float, Float) -> Tree x -> List Form 
-displayTree (x, y) tree =
+displayTree: (Float, Float) -> (Float, Float) -> Int -> Tree x -> List Form 
+displayTree parentCoor (x, y) depth tree =
   case tree of
     Empty ->
       []
     Node v left right ->
-      displayTree (x - 20.0, y - 50.0) left ++ 
-      [nodeForms v (x, y), nodeText v (x,y)] ++
-      displayTree (x + 20.0, y - 50.0) right
+      let 
+          height = 25.0 
+          origWidth = 50.0
+          newWidth = origWidth * (5.0 - toFloat depth)  / 5.0 
+          leftnodeCoor = (x - newWidth, y - height)
+          rightnodeCoor = (x + newWidth, y - height)
+      in
+        displayTree (x, y) leftnodeCoor (depth + 1) left 
+        ++ [nodeForms v parentCoor (x, y), nodeText v (x,y)] 
+        ++ displayTree (x, y) rightnodeCoor (depth + 1) right
 
       
-nodeForms v (x,y) =
-  circle 15.0 |> filled Color.red |> move (x,y)
+nodeForms v parentCoor (x,y) =
+  group [ circle 15.0 |> filled Color.red |> move (x,y)
+        , segment parentCoor (x,y) |> traced defaultLine ]
+
 
 nodeText v (x,y) =  
   v |> toString 
@@ -29,4 +39,4 @@ nodeText v (x,y) =
 
 treeElement: List Int -> Element
 treeElement treeList = 
-  collage 300 300 (displayTree (0,100) (fromList treeList))
+  collage 300 300 (displayTree (0,100) (0,100) 0 (fromList treeList))
